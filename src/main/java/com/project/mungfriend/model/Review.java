f1cb72612bddc63ffe058fa3fce7737be739f6b3
Jun Kim <rlafbf222@naver.com>
2022-07-04 11:38:08 +0900

    Member Entity에 dogProfileImgUrl attribute 추가 (추 후 Dog_CUD 로직에서 대표 강아지 관련 처리 시 해당 attribute에 set 필요)

--- src/main/java/com/project/mungfriend/model/Review.java
+++ src/main/java/com/project/mungfriend/model/Review.java
@@ -29,6 +29,8 @@ public class Review extends Timestamped{
 
     private String giverNickname;
 
+    private String giverDogProfileImgUrl = "";
+
     @ManyToOne
     @JsonIgnore
     @JoinColumn(name="TAKER_ID")
@@ -45,6 +47,7 @@ public class Review extends Timestamped{
     public Review(Member giver, Member taker, PostReviewRequestDto requestDto) {
         this.giver = giver;
         this.giverNickname = giver.getNickname();
+        this.giverDogProfileImgUrl = giver.getDogProfileImgUrl();
 
         this.taker = taker;
 
