54cdb2fe9a0034595437ad8c47801da195e10093
souliat <souliat@naver.com>
2022-07-05 14:51:03 +0900

    리뷰 컨트롤러 @RequestBody 추가

--- src/main/java/com/project/mungfriend/controller/ReviewController.java
+++ src/main/java/com/project/mungfriend/controller/ReviewController.java
@@ -7,10 +7,7 @@ import com.project.mungfriend.security.SecurityUtil;
 import com.project.mungfriend.service.ReviewService;
 import com.project.mungfriend.service.S3Uploader;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -32,7 +29,7 @@ public class ReviewController {
 
     // s3 이미지 삭제 테스트
     @DeleteMapping("/api/reviews")
-    public void deleteImg(DeleteImgRequestDto requestDto){
+    public void deleteImg(@RequestBody DeleteImgRequestDto requestDto){
         reviewService.deleteTest(requestDto.getKey());
     }
 }
