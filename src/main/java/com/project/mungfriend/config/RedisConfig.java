f3f131a28fd332b56367d6a4f539364fc4f43d8d
Jun Kim <rlafbf222@naver.com>
2022-07-19 12:16:42 +0900

    Lettuce Bean 삭제 (인식 불가)

--- src/main/java/com/project/mungfriend/config/RedisConfig.java
+++ src/main/java/com/project/mungfriend/config/RedisConfig.java
@@ -24,11 +24,6 @@ public class RedisConfig {
     //어플리케이션에서 사용할 redisTemplate 설정
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();  // Lettuce 사용
-    }
-
-    @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
