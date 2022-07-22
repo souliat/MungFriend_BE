4edf8befeddd9599703f4a1b1df61e839d713bd5
Jun Kim <rlafbf222@naver.com>
2022-07-22 11:16:56 +0900

    HttpSession 객체 사용하여 정보 저장하기

--- src/main/java/com/project/mungfriend/controller/PhoneCheckController.java
+++ src/main/java/com/project/mungfriend/controller/PhoneCheckController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -24,28 +25,32 @@ public class PhoneCheckController {
     @PostMapping("/phone/auth")
     @ResponseBody
 
-    public Boolean phoneAuth(@RequestBody PhoneCheckRequestDto requestDto, HttpServletRequest request) {
+    public Boolean phoneAuth(@RequestBody PhoneCheckRequestDto requestDto, HttpServletRequest request,
+                             HttpSession session
+                             ) {
 
         if (memberRepository.existsByPhoneNum(requestDto.getPhoneNum())) {
             throw new IllegalArgumentException("이미 등록된 휴대폰 번호입니다.");
         }
 
         String code = PhoneCheckService.sendRandomMessage(requestDto.getPhoneNum());
-        request.getSession().setAttribute("rand", code);
 
-        String rand = (String) request.getSession().getAttribute("rand");
-        request.getCookies();
+//        request.getSession().setAttribute("rand", code);
+        session.setAttribute("rand", code);
+
+        String rand = (String) session.getAttribute("rand");
         System.out.println("세션에 담은 직 후" + rand);
         return true;
     }
 
     @PostMapping("/phone/auth/ok")
     @ResponseBody
-    public Boolean phoneAuthOk(HttpServletRequest request, @RequestBody PhoneCheckOkRequestDto requestDto) {
+    public Boolean phoneAuthOk(HttpServletRequest request, @RequestBody PhoneCheckOkRequestDto requestDto,
+                               HttpSession session) {
 
-        String rand = (String) request.getSession().getAttribute("rand");
+//        String rand = (String) request.getSession().getAttribute("rand");
+        String rand = (String) session.getAttribute("rand");
 
-        System.out.println(request.getSession().getAttribute("rand"));
         System.out.println(rand + " : " + requestDto.getCode());
 
         if (rand.equals(requestDto.getCode())) {
